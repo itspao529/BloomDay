a176e73f9d800dc40646b93e6f076b3dbdc223c9
Kioffrx <entiquezometa@gmail.com>
2026-05-20 23:55:19 -0600

    fix: agregar interceptor de token en API y limpiar headers manuales

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
   baseURL: "http://192.168.1.143:3000/api",
@@ -6,5 +7,16 @@ const API = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+API.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
