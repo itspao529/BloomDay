9887897afcd4e3b762546958bcc0750e71d10022
Ángel Josué <87098138+Jeansito2021@users.noreply.github.com>
2026-05-20 19:33:37 -0600

    Initialize Axios instance for API calls

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,1 +1,13 @@
+import axios from "axios";
 
+const API = axios.create({
+
+  baseURL: "http://192.168.0.11:8080/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+
+});
+
+export default API;
