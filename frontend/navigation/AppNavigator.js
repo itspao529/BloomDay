7f75fc03b8078d1aaf77c75286cf4f4d42fe55d5
Ángel Josué <87098138+Jeansito2021@users.noreply.github.com>
2026-05-20 19:29:38 -0600

    Implement AppNavigator with multiple screens

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -1,1 +1,75 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import SplashScreen from "../screens/SplashScreen";
+
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import HomeScreen from "../screens/HomeScreen";
+import CalendarScreen from "../screens/CalendarScreen";
+import EventScreen from "../screens/EventScreen";
+import TaskScreen from "../screens/TaskScreen";
+import NotificationScreen from "../screens/NotificationScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+
+  return (
+
+    <NavigationContainer>
+
+      <Stack.Navigator initialRouteName="Splash">
+
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          name="Calendar"
+          component={CalendarScreen}
+        />
+
+        <Stack.Screen
+          name="Events"
+          component={EventScreen}
+        />
+
+        <Stack.Screen
+          name="Tasks"
+          component={TaskScreen}
+        />
+
+        <Stack.Screen
+          name="Notifications"
+          component={NotificationScreen}
+        />
+
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
